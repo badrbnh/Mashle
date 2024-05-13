471041e9915551861e91056efc1098e15f20c625
badreddine <badrechahide@gmail.com>
2024-05-13 01:04:49 +0100

    [edit] urls

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -6,7 +6,7 @@ from django.urls import path, include
 urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/v1/", include('MashleAPI.urls')),
-    path("api/v2/payment/", include('MashlePayment.urls')),
+    # path("api/v2/payment/", include('MashlePayment.urls')),
     path('auth/', include('djoser.urls')),
     path('auth/', include('djoser.urls.jwt')),
     # path("", include('MashleBot.urls')),
