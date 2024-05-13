dbe10050a49bab7241e2166a59350b5f4153bdad
badreddine <badrechahide@gmail.com>
2024-05-13 02:31:11 +0100

    [fix] wsgi

--- backend/backend/wsgi.py
+++ backend/backend/wsgi.py
@@ -1,7 +1,6 @@
 import os
-from django.core.wsgi import get_wsgi_application
-
 settings_module = 'backend.deployment' if 'WEBSITE_HOSTNAME' in os.environ else 'backend.settings'
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', settings_module)
+from django.core.wsgi import get_wsgi_application
 
 application = get_wsgi_application()
