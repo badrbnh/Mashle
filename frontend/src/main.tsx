c419be7dd1dfedb4d0bb115a3fab6bf1db92e9e5
badreddine <badrechahide@gmail.com>
2024-05-15 02:56:52 +0100

    [ADD] static config

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,13 +1,16 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import App from "./App.tsx";
 import { Provider } from "react-redux";
 import store from "./app/store";
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+
+const root = document.getElementById('root');
+ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
       <App />
     </Provider>
-  </React.StrictMode>
+  </React.StrictMode>,
+  root
 );
