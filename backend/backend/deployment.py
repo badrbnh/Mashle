92e291e56fb726d4b5e9472fb7880608670ee3b3
badreddine <badrechahide@gmail.com>
2024-05-20 17:38:00 +0100

    fix(deployment): fix csrf verfication error
    I added the backend link to the trusted origins
    
    #3

--- backend/backend/deployment.py
+++ backend/backend/deployment.py
@@ -24,15 +24,18 @@ MIDDLEWARE = [
 CORS_ALLOWED_ORIGINS = [
     'https://www.chi-kara.tech',
     'https://delightful-island-0e566b303.5.azurestaticapps.net',
+    'https://mashle-backend.azurewebsites.net'
 ]
 
 CORS_ORIGIN_WHITELIST = [
     'https://www.chi-kara.tech',
     'https://delightful-island-0e566b303.5.azurestaticapps.net',
+    'https://mashle-backend.azurewebsites.net'
 ]
 CSRF_TRUSTED_ORIGINS = [
     'https://www.chi-kara.tech',
     'https://delightful-island-0e566b303.5.azurestaticapps.net',
+    'https://mashle-backend.azurewebsites.net'
     ]
 
 
