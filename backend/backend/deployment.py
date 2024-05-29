3efb812dea57f5b05e5b81b926255b55bfbf99b3
badreddine <badrechahide@gmail.com>
2024-05-29 09:35:26 +0100

    adding media settings
    adding media setting and renaming the media files
    
    issue: #2

--- backend/backend/deployment.py
+++ backend/backend/deployment.py
@@ -59,4 +59,12 @@ DATABASES = {
     }
 }
 
+STATIC_URL = '/static/'
+
 STATIC_ROOT = BASE_DIR/'staticfiles'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'media'),
+]
+
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
